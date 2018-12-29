ab1b081543bec2096a2925eaa98580c460604ed1
Yi Zhenfei <yizhenfei@outlook.com>
2018-12-29 12:09:47 +0800

    Add cmake-mode

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (lsp-rust lsp-mode yasnippet yaml-mode use-package spinner rust-mode protobuf-mode nlinum monokai-theme magit lua-mode hungry-delete helm-rtags helm-projectile fill-column-indicator exec-path-from-shell company-ycmd 0blayout))))
+    (cmake-mode lsp-rust lsp-mode yasnippet yaml-mode use-package spinner rust-mode protobuf-mode nlinum monokai-theme magit lua-mode hungry-delete helm-rtags helm-projectile fill-column-indicator exec-path-from-shell company-ycmd 0blayout))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
